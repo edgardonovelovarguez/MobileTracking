b9bc409a9d26e1b0828a301e2e5c5b779e2b65c8
edgardo.novelo <edgardo.novelo@galletasdonde.com>
2021-05-20 11:49:58 -0500

    *Creación esquema del proyecto *Implemetación de INotifyPropertyChanged

--- MobileTracking/MobileTracking/App.xaml.cs
+++ MobileTracking/MobileTracking/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MobileTracking.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace MobileTracking
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new PrincipalPage();
         }
 
         protected override void OnStart()
---
+++ MobileTracking/MobileTracking/Mvvm/PrincipalViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MobileTracking.Mvvm
+{
+    public class PrincipalViewModel: INotifyPropertyChanged
+    {
+        public PrincipalViewModel()
+        {
+
+        }
+
+        #region Property Changed
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
